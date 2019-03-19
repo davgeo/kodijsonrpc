2f93afa80e5ef69fb66c5920fa29c6c5fb5bac99
Marco Schlicht <schlicht@itc.rwth-aachen.de>
2019-03-19 09:35:03 +0100

    changed style to comply with pep8

--- kodijsonrpc/kodijsonrpc.py
+++ kodijsonrpc/kodijsonrpc.py
@@ -1,93 +1,73 @@
 #!/usr/bin/env python3
-
-'''
-kodijsonrpc.py
+"""kodijsonrpc.py
 
 Set up Kodi specific JSON RPC client.
 
 All Kodi JSON methods can be called as functions
 to the KodiJSONClient instance.
-
-'''
+"""
 
 from jsonrpcclient.http_server import HTTPServer
 
-KODI_JSON_NAMESPACES = ["VideoLibrary",
-                        "Settings",
-                        "Favourites",
-                        "AudioLibrary",
-                        "Application",
-                        "Player",
-                        "Input",
-                        "System",
-                        "Playlist",
-                        "Addons",
-                        "AudioLibrary",
-                        "Files",
-                        "GUI" ,
-                        "JSONRPC",
-                        "PVR",
-                        "xbmc"]
+KODI_JSON_NAMESPACES = [
+    "VideoLibrary", "Settings", "Favourites", "AudioLibrary", "Application",
+    "Player", "Input", "System", "Playlist", "Addons", "AudioLibrary", "Files",
+    "GUI", "JSONRPC", "PVR", "xbmc"
+]
 
-#################################################
-#
-# KodiNamespaceMethodCatcher
-#
-# This provides a __getattr__ method which
-# catches all method calls and makes the
-# corresponding server requests.
-#
-#################################################
+
 class KodiNamespaceMethodCatcher(object):
-  #################################################
-  # __init__
-  #################################################
-  def __init__(self, server, namespace):
-    self.server = server
-    self.namespace = namespace
+    """KodiNamespaceMethodCatcher
 
-  #################################################
-  # __getattr__ : catch all function calls
-  #################################################
-  def __getattr__(self, function):
-    def func(*args, **kwargs):
-      return self.server.request("{0}.{1}".format(self.namespace, function), *args, **kwargs)
-    return func
+    This provides a __getattr__ method which
+    catches all method calls and makes the
+    corresponding server requests.
+    """
 
-#################################################
-#
-# KodiJSONClient
-#
-# Insantiate KodiNamespaceMethodCatcher classes
-# for all Kodi JSON namespaces. This allows any
-# of the Kodi JSON methods to be called as methods
-# of the KodiJSONClient
-#
-# e.g <KodiJSONClient object>.JSONRPC.Ping()
-# would result in a ping request sent to the
-# configured kodi server.
-#
-#################################################
+    def __init__(self, server, namespace):
+        """__init__"""
+        self.server = server
+        self.namespace = namespace
+
+    def __getattr__(self, function):
+        """__getattr__ : catch all function calls"""
+        def func(*args, **kwargs):
+            return self.server.request(
+                "{0}.{1}".format(self.namespace, function), *args, **kwargs)
+
+        return func
+
+
 class KodiJSONClient(object):
-  #################################################
-  # __init__
-  #################################################
-  def __init__(self, host, port, user, pwd):
-    self.url = 'http://{0}:{1}/'.format(host, port)
-    self.server = HTTPServer(self.url + 'jsonrpc', headers={'content-type': 'application/json'}, auth=(user, pwd))
-    for namespace in KODI_JSON_NAMESPACES:
-      self.__dict__[namespace] = KodiNamespaceMethodCatcher(self.server, namespace)
+    """KodiJSONClient
 
-#################################################
-#
-# EnableJSONLogging
-#
-# Enable logging on jsconrpcclient module
-#
-#################################################
-def EnableJSONLogging():
-  import logging
-  logging.getLogger('jsonrpcclient').setLevel(logging.INFO)
-  logging.basicConfig()
+    Insantiate KodiNamespaceMethodCatcher classes
+    for all Kodi JSON namespaces. This allows any
+    of the Kodi JSON methods to be called as methods
+    of the KodiJSONClient
+
+    e.g <KodiJSONClient object>.JSONRPC.Ping()
+    would result in a ping request sent to the
+    configured kodi server.
+    """
+
+    def __init__(self, host, port, user, pwd):
+        """__init__"""
+        self.url = 'http://{0}:{1}/'.format(host, port)
+        self.server = HTTPServer(
+            self.url + 'jsonrpc',
+            headers={'content-type': 'application/json'},
+            auth=(user, pwd))
+        for namespace in KODI_JSON_NAMESPACES:
+            self.__dict__[namespace] = KodiNamespaceMethodCatcher(
+                self.server, namespace)
 
 
+def enable_json_logging():
+    """EnableJSONLogging
+
+    Enable logging on jsconrpcclient module
+    """
+    import logging
+    logging.getLogger('jsonrpcclient').setLevel(logging.INFO)
+    logging.basicConfig()
